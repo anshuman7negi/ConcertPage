7616ab115ed56d94e0096395e1998adad837968b
anshuman7negi <anshumannegi777@gmail.com>
2023-06-26 10:27:57 +0530

    Added functionality to more btton in guest section

--- script.js
+++ script.js
@@ -32,3 +32,47 @@ for (let i = 0; i < projectCards.length; i += 1) {
                          </p>
                         </div>`;
 };
+
+const singers = [
+    {
+        singerImage: 'images/sun.jpg',
+        singerName: 'Arjit Singh',
+        singerTitle: "He is very good singer he have almost released 300 songs",
+        singerDetail: 'Benkler has relased more than 300 songs, and he only sing in hindi language.'
+    },
+    {
+        singerImage: 'images/sun.jpg',
+        singerName: 'Arjit Singh',
+        singerTitle: "He is very good singer he have almost released 300 songs",
+        singerDetail: 'Benkler has relased more than 300 songs, and he only sing in hindi language.'
+    },
+    {
+        singerImage: 'images/sun.jpg',
+        singerName: 'Arjit Singh',
+        singerTitle: "He is very good singer he have almost released 300 songs",
+        singerDetail: 'Benkler has relased more than 300 songs, and he only sing in hindi language.'
+    },
+    {
+        singerImage: 'images/sun.jpg',
+        singerName: 'Arjit Singh',
+        singerTitle: "He is very good singer he have almost released 300 songs",
+        singerDetail: 'Benkler has relased more than 300 songs, and he only sing in hindi language.'
+    },
+];
+
+function createSingerCard() {
+    console.log("hellow");
+    const guest = document.getElementById('guest-list');
+    for (let i = 0; i < singers.length; i += 1) {
+        guest.innerHTML += `<div class="singer">
+                            <img src="images/singer1.png" alt="">
+                            <div class="singer-details">
+                               <h3>${singers[i].singerName}</h3>
+                               <h4>${singers[i].singerTitle}</h4>
+                               <p>${singers[i].singerDetail}</p>
+                           </div>`
+    }
+    document.getElementById('seeMore').style.display = 'none';
+}
+
+document.getElementById('seeMore').addEventListener('click', createSingerCard);
